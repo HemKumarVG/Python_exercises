18f902eb74132fed6e0787935d0e46ce818e7309
Hem Kumar VG <hem.kumarvg@gmail.com>
2025-06-26 08:44:01 +0530

    Program to Guess the lucky number

---
+++ lucky.py
@@ -0,0 +1,13 @@
+lucky = 9
+guess = int(input("Guess any random number between 1 to 25:"))
+
+if guess >= lucky + 3:
+    print("you guess is too high")
+
+if guess <= lucky - 3:
+    print("your guess is too low")
+
+if guess == lucky:
+    print("congratulations you guessed it right")
+elif (guess >= lucky + 2) and (guess <= lucky + 2):
+    print("you allmost there")
