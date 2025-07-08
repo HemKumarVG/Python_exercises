9ea0659a78f50b3124bff3f5ee438af7458551b3
Hem Kumar VG <hem.kumarvg@gmail.com>
2025-07-08 08:06:37 +0530

    Multiplication table for any number

---
+++ Multiplication.py
@@ -0,0 +1,18 @@
+#Multiplication table of any number desired by the user
+
+
+def mul_table(num):
+    print (num * 1)
+    print (num * 2)
+    print (num * 3)
+    print (num * 4)
+    print (num * 5)
+    print (num * 6)
+    print (num * 7)
+    print (num * 8)
+    print (num * 9)
+    print (num * 10)
+        
+        
+usr_val = int(input("Enter the number for the multiplication: "))
+mul_table(usr_val)
