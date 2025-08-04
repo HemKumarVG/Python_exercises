88721d4d93fc6314bc2b8f5ec8d077d33909b37a
Hem Kumar VG <hem.kumarvg@gmail.com>
2025-08-04 20:06:33 +0530

    Add files via upload

--- Multiplication.py
+++ Multiplication.py
@@ -2,16 +2,17 @@
 
 
 def mul_table(num):
-    print (num, "x 1 =", num * 1)
-    print (num * 2)
-    print (num * 3)
-    print (num * 4)
-    print (num * 5)
-    print (num * 6)
-    print (num * 7)
-    print (num * 8)
-    print (num * 9)
-    print (num * 10)
+    #print (num, "x  1  =","", num * 1)
+    print (num, "x  1  = ", num * 1)
+    print (num, "x  2  =", num * 2)
+    print (num, "x  3  =", num * 3)
+    print (num, "x  4  =", num * 4)
+    print (num, "x  5  =", num * 5)
+    print (num, "x  6  =", num * 6)
+    print (num, "x  7  =", num * 7)
+    print (num, "x  8  =", num * 8)
+    print (num, "x  9  =", num * 9)
+    print (num, "x 10  =", num * 10)
         
         
 usr_val = int(input("Enter the number for the multiplication: "))
