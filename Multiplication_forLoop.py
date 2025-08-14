59692d2074adbd37b5163e6f3dae727113ab51c0
Hem Kumar VG <hem.kumarvg@gmail.com>
2025-08-14 09:15:27 +0530

    Add files via upload

---
+++ Multiplication_forLoop.py
@@ -0,0 +1,7 @@
+#for loop for multiply
+
+num = 17
+
+for val in range (1, 11):
+    print (num, "x", val,  "=", num * val)
+
---
+++ Multiplication_whileLoop.py
@@ -0,0 +1,24 @@
+#while loop for multiply
+
+num = 17
+
+"""
+This codes run from 0 to 9
+
+count = 0
+while count < 10:
+    print (num, "x", count,  "=", num * count)
+    count = count + 1
+""""
+
+#this code starts from 1 to 10
+
+count = 1
+while count < 11:
+    print (num, "x", count,  "=", num * count)
+    count = count + 1
+    
+    
+
+    
+
